33db12be0936304ba47e50c36f4f225b768ecbb9
dyxs-site <mail@dyxs.site>
2024-02-05 17:27:29 +0800

    优化 dianying.html

--- app/controllers/web/v1/VideosController.go
+++ app/controllers/web/v1/VideosController.go
@@ -2,6 +2,10 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	cmap "github.com/orcaman/concurrent-map"
+	"goapi/app/models"
+	"goapi/app/service"
+	"goapi/pkg/mysql"
 	"net/http"
 )
 
@@ -10,14 +14,150 @@ type VideosController struct {
 }
 
 func (h *VideosController) Dianying(c *gin.Context) {
+	table := "dianying.html"
+	DianYingData := cmap.New().Items()
 	value, exists := c.Get("data")
 	if !exists {
 		value = gin.H{}
 	}
-	data := value.(gin.H)
-	data["page"] = "dianying"
-	data["title"] = "电影"
-	c.HTML(http.StatusOK, "v/dianying.html", data)
+	DATA := value.(gin.H)
+
+	// 批量查询数据
+	var (
+		listMacType []models.MacType
+		CurrentlyTrending, listDongZuoPian,
+		listXiJuPian, listAiQingPian,
+		listKeHuanPian, listKongBuPian,
+		listJuQingPian, listZhanZhengPian,
+		listDongHuaPian, listQiHuanPian,
+		listJiLuPian []models.MacVod
+	)
+	MacVod := models.MacVodMgr(mysql.DB)
+	MacType := models.MacTypeMgr(mysql.DB)
+
+	// 电影详细分类
+	CacheMacType, _ := service.GetTable(table, "listMacType", []models.MacType{})
+	if CacheMacType == nil {
+		MacType.Debug().Where("type_pid", 1).Where("type_status", 1).Order("type_sort desc").Find(&listMacType)
+		service.SaveTable(table, "listMacType", listMacType)
+	} else {
+		listMacType = *CacheMacType.(*[]models.MacType)
+	}
+
+	// 正在热播
+	CacheCurrentlyTrending, _ := service.GetTable(table, "CurrentlyTrending", []models.MacVod{})
+	if CacheCurrentlyTrending == nil {
+		MacVod.Debug().Where("type_id_1", 1).Where("vod_status", 1).Order("vod_hits desc").Limit(16).Find(&CurrentlyTrending)
+		service.SaveTable(table, "CurrentlyTrending", CurrentlyTrending)
+	} else {
+		CurrentlyTrending = *CacheCurrentlyTrending.(*[]models.MacVod)
+	}
+
+	// 动作片
+	CacheDongZuoPian, _ := service.GetTable(table, "listDongZuoPian", []models.MacVod{})
+	if CacheDongZuoPian == nil {
+		MacVod.Debug().Where("type_id", 6).Order("vod_hits desc").Limit(14).Find(&listDongZuoPian)
+		service.SaveTable(table, "listDongZuoPian", listDongZuoPian)
+	} else {
+		listDongZuoPian = *CacheDongZuoPian.(*[]models.MacVod)
+	}
+
+	// 喜剧片
+	CacheXiJu, _ := service.GetTable(table, "listXiJuPian", []models.MacVod{})
+	if CacheXiJu == nil {
+		MacVod.Debug().Where("type_id", 7).Order("vod_hits desc").Limit(14).Find(&listXiJuPian)
+		service.SaveTable(table, "listXiJuPian", listXiJuPian)
+	} else {
+		listXiJuPian = *CacheXiJu.(*[]models.MacVod)
+	}
+
+	// 爱情片
+	CacheAiQingPian, _ := service.GetTable(table, "listAiQingPian", []models.MacVod{})
+	if CacheAiQingPian == nil {
+		MacVod.Debug().Where("type_id", 8).Order("vod_hits desc").Limit(14).Find(&listAiQingPian)
+		service.SaveTable(table, "listAiQingPian", listAiQingPian)
+	} else {
+		listAiQingPian = *CacheAiQingPian.(*[]models.MacVod)
+	}
+
+	// 科幻片
+	CacheKeHuanPian, _ := service.GetTable(table, "listKeHuanPian", []models.MacVod{})
+	if CacheKeHuanPian == nil {
+		MacVod.Debug().Where("type_id", 9).Order("vod_hits desc").Limit(14).Find(&listKeHuanPian)
+		service.SaveTable(table, "listKeHuanPian", listKeHuanPian)
+	} else {
+		listKeHuanPian = *CacheKeHuanPian.(*[]models.MacVod)
+	}
+
+	// 恐怖片
+	CacheKongBuPian, _ := service.GetTable(table, "listKongBuPian", []models.MacVod{})
+	if CacheKongBuPian == nil {
+		MacVod.Debug().Where("type_id", 10).Order("vod_hits desc").Limit(14).Find(&listKongBuPian)
+		service.SaveTable(table, "listKongBuPian", listKongBuPian)
+	} else {
+		listKongBuPian = *CacheKongBuPian.(*[]models.MacVod)
+	}
+
+	// 剧情片
+	CacheJuQingPian, _ := service.GetTable(table, "listJuQingPian", []models.MacVod{})
+	if CacheJuQingPian == nil {
+		MacVod.Debug().Where("type_id", 11).Order("vod_hits desc").Limit(14).Find(&listJuQingPian)
+		service.SaveTable(table, "listJuQingPian", listJuQingPian)
+	} else {
+		listJuQingPian = *CacheJuQingPian.(*[]models.MacVod)
+	}
+
+	// 战争片
+	CacheZhanZhengPian, _ := service.GetTable(table, "listZhanZhengPian", []models.MacVod{})
+	if CacheZhanZhengPian == nil {
+		MacVod.Debug().Where("type_id", 12).Order("vod_hits desc").Limit(14).Find(&listZhanZhengPian)
+		service.SaveTable(table, "listZhanZhengPian", listZhanZhengPian)
+	} else {
+		listZhanZhengPian = *CacheZhanZhengPian.(*[]models.MacVod)
+	}
+
+	// 动画片
+	CacheDongHuaPian, _ := service.GetTable(table, "listDongHuaPian", []models.MacVod{})
+	if CacheDongHuaPian == nil {
+		MacVod.Debug().Where("type_id", 20).Order("vod_hits desc").Limit(14).Find(&listDongHuaPian)
+		service.SaveTable(table, "listDongHuaPian", listDongHuaPian)
+	} else {
+		listDongHuaPian = *CacheDongHuaPian.(*[]models.MacVod)
+	}
+
+	// 奇幻片
+	CacheQiHuanPian, _ := service.GetTable(table, "listQiHuanPian", []models.MacVod{})
+	if CacheQiHuanPian == nil {
+		MacVod.Debug().Where("type_id", 21).Order("vod_hits desc").Limit(14).Find(&listQiHuanPian)
+		service.SaveTable(table, "listQiHuanPian", listQiHuanPian)
+	} else {
+		listQiHuanPian = *CacheQiHuanPian.(*[]models.MacVod)
+	}
+
+	// 记录片
+	CacheJiLuPian, _ := service.GetTable(table, "listJiLuPian", []models.MacVod{})
+	if CacheJiLuPian == nil {
+		MacVod.Debug().Where("type_id", 22).Order("vod_hits desc").Limit(14).Find(&listJiLuPian)
+		service.SaveTable(table, "listJiLuPian", listJiLuPian)
+	} else {
+		listJiLuPian = *CacheJiLuPian.(*[]models.MacVod)
+	}
+
+	DianYingData["listMacType"] = listMacType
+	DianYingData["CurrentlyTrending"] = CurrentlyTrending
+	DianYingData["listDongZuoPian"] = listDongZuoPian
+	DianYingData["listXiJuPian"] = listXiJuPian
+	DianYingData["listAiQingPian"] = listAiQingPian
+	DianYingData["listKeHuanPian"] = listKeHuanPian
+	DianYingData["listKongBuPian"] = listKongBuPian
+	DianYingData["listJuQingPian"] = listJuQingPian
+	DianYingData["listZhanZhengPian"] = listZhanZhengPian
+	DianYingData["listDongHuaPian"] = listDongHuaPian
+	DianYingData["listQiHuanPian"] = listQiHuanPian
+	DianYingData["listJiLuPian"] = listJiLuPian
+	DATA["DianYingData"] = DianYingData
+	DATA["page"] = "dianying"
+	c.HTML(http.StatusOK, "v/dianying.html", DATA)
 }
 
 func (h *VideosController) Dianshiju(c *gin.Context) {
