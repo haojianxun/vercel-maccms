0f2b7960626a57922f7e4accc5a264e2bd4636cd
dyxs-site <mail@dyxs.site>
2024-02-23 16:41:18 +0800

    优化Mysql最大的存活时间，每个链接的过期时间

--- config/database.go
+++ config/database.go
@@ -19,7 +19,7 @@ func init() {
 			// 连接池配置
 			"max_idle_connections": config.Env("DB_MAX_IDLE_CONNECTIONS", 25),
 			"max_open_connections": config.Env("DB_MAX_OPEN_CONNECTIONS", 100),
-			"max_life_seconds":     config.Env("DB_MAX_LIFE_SECONDS", 5*60),
+			"max_life_seconds":     config.Env("DB_MAX_LIFE_SECONDS", 60),
 		},
 	})
 }
