4a9b81771685aa71ce670891c9e705bd0ac022be
dyxs-site <mail@dyxs.site>
2024-03-07 18:40:16 +0800

    menus

--- app/service/site.go
+++ app/service/site.go
@@ -3,14 +3,21 @@ package service
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"goapi/app/models"
 )
 
 func Site(c *gin.Context) gin.H {
-	data := gin.H{}
+	PageData := gin.H{}
 	HTTP := "https://"
-	data["__URL__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, c.Request.URL.String())
-	data["__STATIC__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics")
-	data["__CSS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/css")
-	data["__JS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/js")
-	return data
+	PageData["__URL__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, c.Request.URL.String())
+	PageData["__STATIC__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics")
+	PageData["__CSS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/css")
+	PageData["__JS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/js")
+
+	// 公共数据
+	// 查询所有分类
+	var NavMenus []models.MacType
+	ListMacType("common", -1, &NavMenus)
+	PageData["NavMenus"] = NavMenus
+	return PageData
 }
