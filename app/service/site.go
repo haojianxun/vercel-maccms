9f44a3872488f718c8663fcf4fc468c70d297a9a
dyxs-site <mail@dyxs.site>
2024-03-16 16:58:26 +0800

    调整静态资源文件夹

--- app/service/site.go
+++ app/service/site.go
@@ -10,9 +10,10 @@ func Site(c *gin.Context) gin.H {
 	PageData := gin.H{}
 	HTTP := "https://"
 	PageData["__URL__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, c.Request.URL.String())
-	PageData["__STATIC__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics")
-	PageData["__CSS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/css")
-	PageData["__JS__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, "/statics/js")
+	HttpStatic := "https://static.dyxs.site"
+	PageData["__STATIC__"] = HttpStatic
+	PageData["__CSS__"] = fmt.Sprintf("%s%s", HttpStatic, "/css")
+	PageData["__JS__"] = fmt.Sprintf("%s%s", HttpStatic, "/js")
 
 	var NavMenus []models.MacType
 	ListMacType("NavMenus", 0, &NavMenus)
