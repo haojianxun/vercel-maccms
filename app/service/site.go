37ea9f62903b987c5e51352a2b0822692e424aa9
dyxs-site <mail@dyxs.site>
2024-03-18 09:56:34 +0800

    优化站点资源

--- app/service/site.go
+++ app/service/site.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"goapi/app/models"
+	"strings"
 )
 
 func Site(c *gin.Context) gin.H {
 	PageData := gin.H{}
-	HTTP := "https://"
-	PageData["__URL__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, c.Request.URL.String())
 	HttpStatic := "https://static.dyxs.site"
+	HTTP := "https://"
+	if strings.Contains(c.Request.Host, "localhost") || strings.Contains(c.Request.Host, "127.0.0.1") {
+		HTTP = "http://"
+		HttpStatic = fmt.Sprintf("%s%s/statics", HTTP, c.Request.Host)
+	}
+	PageData["__URL__"] = fmt.Sprintf("%s%s%s", HTTP, c.Request.Host, c.Request.URL.String())
 	PageData["__STATIC__"] = HttpStatic
 	PageData["__CSS__"] = fmt.Sprintf("%s%s", HttpStatic, "/css")
 	PageData["__JS__"] = fmt.Sprintf("%s%s", HttpStatic, "/js")
